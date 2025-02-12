fe72f9004c678a516b744dd70d6978a9a00efe49
Shiv209 <114733037+Shiv209@users.noreply.github.com>
2025-02-12 22:31:53 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
-    await mongoose.connect('mongodb+srv://shivkumarainwad:G8VmhB54lCRFsHUO@cluster1.dql2w.mongodb.net/?retryWrites=true&w=majority&appName=Cluster1').then(()=>console.log("DataBase Connected"));
+    await mongoose.connect('your_Mongo_URI').then(()=>console.log("DataBase Connected"));
 }
