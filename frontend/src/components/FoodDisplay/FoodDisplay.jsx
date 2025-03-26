0a154cd80cddf7618a69b23572aaac5f5da897fb
Shiv209 <114733037+Shiv209@users.noreply.github.com>
2025-03-26 23:33:29 +0530

    Update FoodDisplay.jsx

--- frontend/src/components/FoodDisplay/FoodDisplay.jsx
+++ frontend/src/components/FoodDisplay/FoodDisplay.jsx
@@ -5,7 +5,7 @@ import FoodItem from '../FoodItem/FoodItem'
 
 const FoodDisplay = ({category}) => {
 
-    const {food_list} = useContext(StoreContext)
+    const {food_list} = useContext(StoreContext);
   return (
     <div className='food-display' id='food-display'>
         <h2 className='h2we'>Top dishes near you</h2>
